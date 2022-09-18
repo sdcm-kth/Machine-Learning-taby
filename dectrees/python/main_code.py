2aef93077d769550b887b2d525cc475ca6422fc5
SarD-CM <A01652414@tec.mx>
2022-09-18 14:40:26 +0200

    Assignment 7 again

--- dectrees/python/main_code.py
+++ dectrees/python/main_code.py
@@ -5,7 +5,6 @@ import dtree as dt
 import numpy as np
 import random
 import drawtree_qt5 as dwt5
-from prettytable import PrettyTable
 import matplotlib.pyplot as plt
 import drawtree_qt5 as qt
 
@@ -60,24 +59,24 @@ def partition(data, fraction):
   breakPoint = int(len(ldata) * fraction)
   return ldata[:breakPoint], ldata[breakPoint:]
 
-def prune_trees(data, test):
+def prune_tree(data, test):
   fractions = [0.3, 0.4, 0.5, 0.6, 0.7, 0.8]
   pruned = []
 
   for fraction in fractions:
     train, validate = partition(data, fraction)
-    tree = dt.buildTree(train, m.attributes)
-    forest = dt.allPruned(tree)
-    best_perf = dt.check(tree, validate)
+    t = dt.buildTree(train, m.attributes)
+    prun = dt.allPruned(t) # See all the posibilities
+    best_perf = dt.check(t, validate)
 
     temp_tree = 0
-    best_tree = tree
+    best_tree = t
 
-    for t in forest:
-      temp_perf = dt.check(t, validate)
+    for x in prun:
+      temp_perf = dt.check(x, validate)
       if best_perf < temp_perf:
         best_perf = temp_perf
-        best_tree = t
+        best_tree = x
 
     pruned.append(1 - dt.check(best_tree, test))
 
@@ -88,8 +87,8 @@ monk1_pruned = []
 monk3_pruned = []
 
 for i in range(100):
-    monk1_pruned.append(prune_trees(m.monk1, m.monk1test))
-    monk3_pruned.append(prune_trees(m.monk3, m.monk3test))
+    monk1_pruned.append(prune_tree(monks[0], monks_test[0])) # 
+    monk3_pruned.append(prune_tree(monks[2], monks_test[2]))
 
 monk1_pruned = np.transpose(monk1_pruned)
 monk3_pruned = np.transpose(monk3_pruned)
@@ -99,26 +98,17 @@ mean3 = np.mean(monk3_pruned, axis=1)
 std1 = np.std(monk1_pruned, axis=1)
 std3 = np.std(monk3_pruned, axis=1)
 
-MONK1_mean = np.around(mean1, decimals=6)
-MONK1_std  = np.around(std1, decimals=6)
-MONK3_mean = np.around(mean3, decimals=6)
-MONK3_std  = np.around(std3, decimals=6) 
-print("Monk1 mean: ")
-print(MONK1_mean)
-print("Monk1 STD: ")
-print(MONK1_std)
-print("Monk3 mean: ")
-print(MONK3_mean)
-print("Monk3 STD: ")
-print(MONK3_std)
+MONK1_mean = np.around(mean1, decimals=4); MONK1_std  = np.around(std1, decimals=4)
+MONK3_mean = np.around(mean3, decimals=4); MONK3_std  = np.around(std3, decimals=4) 
+print("MONK-1 mean: {} \n STD: {} \n".format(MONK1_mean,MONK1_std))
+print("MONK-2 mean: {} \n STD: {}".format(MONK1_mean,MONK1_std))
 
-complete_tree1 = dt.buildTree(m.monk1, m.attributes)
-complete_tree3 = dt.buildTree(m.monk3, m.attributes)
-Error1 = [1 - dt.check(complete_tree1, m.monk1test), np.amin(mean1)]
-Error3 = [1 - dt.check(complete_tree3, m.monk3test), np.amin(mean3)]
-print("Error on complete tree vs error on pruned tree")
-print(Error1)
-print(Error3)
+
+complete_tree1 = dt.buildTree(monks[0], m.attributes)
+complete_tree3 = dt.buildTree(monks[2], m.attributes)
+Error1 = [1 - dt.check(complete_tree1, monks_test[0]), np.amin(mean1)]
+Error3 = [1 - dt.check(complete_tree3, monks_test[2]), np.amin(mean3)]
+print("Error on complete tree vs error on pruned tree: \n MONK-1: {} \n MONK-3: {}\n".format(Error1,Error3))
 
 plt.figure()
 plt.title('Mean of Errors')
